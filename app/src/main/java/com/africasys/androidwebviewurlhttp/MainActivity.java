4b275baf3d0995a4603f029dc668b11b668585f0
Relwendé Zoundi <relwendezoundi295@gmail.com>
2022-10-13 14:58:29 +0000

    up

--- app/src/main/java/com/africasys/androidwebviewurlhttp/MainActivity.java
+++ app/src/main/java/com/africasys/androidwebviewurlhttp/MainActivity.java
@@ -2,12 +2,34 @@ package com.africasys.androidwebviewurlhttp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.google.com");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
